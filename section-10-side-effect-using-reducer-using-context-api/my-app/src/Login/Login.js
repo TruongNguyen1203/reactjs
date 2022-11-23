3f62374d363df02ce5e5a8d3364cdd1a31ba2fec
TruongNguyen1203 <nguyentruong.120300@gmail.com>
2022-11-23 23:23:55 +0700

    section 10: useEffect & dependencies & clean up function

--- section-10-side-effect-using-reducer-using-context-api/my-app/src/Login/Login.js
+++ section-10-side-effect-using-reducer-using-context-api/my-app/src/Login/Login.js
@@ -1,21 +1,52 @@
 import "./Login.css";
+import { useEffect, useState } from "react";
 const Login = (props) => {
+  const [enterEmail, setEnterEmail] = useState("");
+  const [enterPassword, setEnterPassword] = useState("");
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const handlerEmailChange = (e) => {
+    setEnterEmail(e.target.value);
+  };
+
+  const handlerPasswordChange = (e) => {
+    setEnterPassword(e.target.value);
+  };
   const handleLogin = (e) => {
     e.preventDefault();
-    console.log('n')
+    console.log("n");
     props.onLogin();
   };
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log("Check valid form");
+      if (enterEmail.includes("@") && enterPassword.length > 6) {
+        console.log("true");
+        setIsFormValid(true);
+      }
+    }, 500);
+
+    return () => {
+      console.log("ClEANUP");
+      clearTimeout(identifier);
+    }
+  }, [enterEmail, enterPassword]);
   return (
     <form className="login-form">
       <div className="input-control">
         <label>Email</label>
-        <input type="email" name="email"></input>
+        <input type="email" name="email" onChange={handlerEmailChange}></input>
       </div>
       <div className="input-control">
         <label>Password</label>
-        <input type="password" name="password"></input>
+        <input
+          type="password"
+          name="password"
+          onChange={handlerPasswordChange}
+        ></input>
       </div>
-      <button type="submit" onClick={handleLogin}>
+      <button type="submit" onClick={handleLogin} disabled={!isFormValid}>
         Login
       </button>
     </form>
